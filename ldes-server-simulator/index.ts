aa5e191a54c08733f697144b3a46b5accf0ec8ed
Ranko Orlic <ranko.orlic@cegeka.com>
2022-05-13 18:06:45 +0200

    added LDES server simulator

---
+++ ldes-server-simulator/index.ts
@@ -0,0 +1,63 @@
+import fastify from 'fastify'
+import minimist from 'minimist'
+import { LdesFragmentRepository } from './ldes-fragment-repository';
+import { TreeNode } from './tree-specification';
+
+const server = fastify();
+const args = minimist(process.argv.slice(2));
+console.debug("arguments: ", args);
+
+const baseUrl = new URL(args.baseUrl || 'http://localhost:8080')
+const repository = new LdesFragmentRepository(baseUrl);
+const redirections: any = {};
+
+server.get('/', async (_request, _reply) => {
+  return { aliases: Object.keys(redirections), fragments: repository.fragments };
+});
+
+server.get('/*', async (request, reply) => {
+  const fragmentId = request.url;
+  const redirection = redirections[fragmentId];
+  if (redirection !== undefined) {
+    return reply.redirect(redirection);
+  }
+
+  const body = repository.get(fragmentId);
+  reply.statusCode = body === undefined ? 404 : 200;
+  return reply.send(body);
+});
+
+server.post('/ldes', async (request, reply) => {
+  reply.statusCode = 201;
+  return { path: repository.store(request.body as TreeNode) };
+});
+
+interface Redirection {
+  original: string;
+  alias: string;
+}
+
+function withoutOrigin(path: string): string {
+  const url = new URL(path);
+  return path.replace(`${url.protocol}//${url.host}`, '');
+}
+
+server.post('/redirect', async (request, _reply) => {
+  const redirection = request.body as Redirection;
+  const original = withoutOrigin(redirection.original);
+  const alias = withoutOrigin(redirection.alias);
+  redirections[alias] = original;
+  return { redirect: { from: alias, to: original } };
+});
+
+const options = { port: Number.parseInt(baseUrl.port), host: baseUrl.hostname };
+server.listen(options, async (err, address) => {
+  if (args.seed) {
+    await repository.seed(args.seed);
+  }
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+  console.log(`Server listening at ${address}`)
+});
